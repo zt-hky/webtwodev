ef262a9397996c2a1aa6fdd6ab898ed9649bd129
TrucMy <nguyentrantrucmy1998@gmail.com>
2019-06-03 12:33:22 +0700

    update Calender and BreadCumb Component in Film page

---
+++ client/src/components/BreadCrumb.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './BreadCrumb.css'
+
+export default class BreadCrumb extends React.Component {
+    render() {
+        return (
+            <nav className="Link">
+                <ul>
+                    <li className="Home">
+                        <a href='/#' ><img style={{width: 20, height:20}} src="../img/nav/home.svg" alt=""/></a>
+                        <span>/ </span>
+                    </li>
+                    <li>
+                        <a href='/#'>Phim</a>
+                        <span>/ </span>
+                    </li>
+                    <li>
+                        <a href='/#'>Phim Đang Chiếu</a>
+                        <span>/ </span>
+                    </li>
+                    <li>
+                        <a href='/#'><strong><u>Aladin</u> </strong> </a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
