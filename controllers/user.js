2ea742224e1fe8e2a9daeaf23e0cad70aede2784
HongKy <hongkynguyen156@gmail.com>
2019-06-09 22:03:05 +0700

    Update controller.user

--- controllers/user.js
+++ controllers/user.js
@@ -5,88 +5,96 @@ const bcrypt = require('bcrypt');
 
 let user = {}
 
-user.signIn = async (req, res, next) => {
+user.signIn = async(req, res, next) => {
     res.status(200);
-    res.setHeader('Content-Type','')
+    res.setHeader('Content-Type', '')
     res.json({
         success: 'true',
         message: 'test'
     });
-   
+
 };
 
-user.signUp = async (req,res,next) => {
+user.signUp = async(req, res, next) => {
 
     // get body
-    const {username,password, name, email, phone} = req.body;
-    
-    if ( !(username && password && name && email && phone))
-    {
+    const { username, password, name, email, phone } = req.body;
+
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var strongPasswordRegex = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+    var vnPhoneNumberRegex = /^(0[3|5|7|8|9])+([0-9]{8})$/;
+
+    if (!(username && password && name && email && phone) ||
+        !emailRegex.test(email) ||
+        !strongPasswordRegex.test(password) ||
+        !vnPhoneNumberRegex.test(phone)
+    ) {
         res.status(400);
         res.json({
-            message : "Not enough body",
-            error : true
+            message: "Unqualified body",
+            error: true
         });
         return;
     }
 
+
     // check exists username
     var users = await models.User.findOne({
-        where: {id : username}
+        where: { id: username }
     });
 
-    if(users){
+    if (users) {
         res.status(400);
         res.json({
-            message : "Username is exists",
-            error : "username"
+            message: "Username is exists",
+            error: "username"
         });
         return;
     }
 
     // check email
     var users = await models.User.findOne({
-        where: {email}
+        where: { email }
     });
 
-    if(users){
+    if (users) {
         res.status(400);
         res.json({
-            message : "Email is exists",
-            error : "email"
+            message: "Email is exists",
+            error: "email"
         });
         return;
     }
 
-    var passwordBcrypt = bcrypt.hashSync(password,10);
-    
+    var passwordBcrypt = bcrypt.hashSync(password, 10);
+
     models.User.create({
-        id : username,
-        password : passwordBcrypt,
+        id: username,
+        password: passwordBcrypt,
         name,
         email,
         phone
     }).
-        then((result) => {
+    then((result) => {
             res.status(200);
             res.json({
-                message : "SignUp successful",
-                error : false
+                message: "SignUp successful",
+                error: false
             });
         })
-        .catch((e)=> {
+        .catch((e) => {
             res.status(400);
             res.json({
-                message : "Error",
-                error : e
+                message: "Error",
+                error: e
             })
         })
-    
-}
-
-user.signOut = (req,res,next) => {
 
 }
 
+user.signOut = (req, res, next) => {
 
-module.exports =  user;
+}
+
+
+module.exports = user;
